0f2b73ace682c39434adbf8ff52fc29abf946dda
nickdandave <nickdandavidson@gmail.com>
2021-03-07 01:20:37 -0500

    Debugging initial function

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,12 +28,15 @@ document.addEventListener('DOMContentLoaded', function() {
                     function handleColorStack() {
                         $window.on("scroll", function watchScroll() {
                             let scrollPos = window.scrollY;
+                            console.log(scrollPos);
 
                             if (scrollPos >= stackPosition && !colorStack.hasClass("fixed-position")) {
                                 colorStack.addClass("fixed-position");
+                                console.log("ADDED");
                             }
                             if (scrollPos < stackPosition && colorStack.hasClass("fixed-position")) {
                                 colorStack.removeClass("fixed-position");
+                                console.log("REMOVED");
                             }
                         });
                     }
